8f8ed9ff06d616dfad1bb3cba469497543359887
Steve Hook <steve.hook@gmail.com>
2016-04-04 19:03:13 +0100

    Write a failing test for POST /api/tasks
    Need to set content-type header here because we are passing a JSON body

--- test/controllers/task_controller_test.exs
+++ test/controllers/task_controller_test.exs
@@ -3,10 +3,14 @@ defmodule Todo.TaskControllerTest do
   alias Todo.Task
   alias Todo.Repo
 
-  test "GET /api/tasks returns a list of tasks" do
+  def create_task do
     { :ok, task } = %Task{title: "Walk the dog"}
       |> Repo.insert
-    tasks_as_json = task
+    task
+  end
+
+  test "GET /api/tasks returns a list of tasks" do
+    tasks_as_json = create_task
       |> List.wrap
       |> Poison.encode!
 
@@ -16,13 +20,23 @@ defmodule Todo.TaskControllerTest do
   end
 
   test "GET /api/tasks/:id returns a single task" do
-    { :ok, task } = %Task{title: "Walk the dog"}
-      |> Repo.insert
+    task = create_task
     task_as_json = task
       |> Poison.encode!
 
     conn = get conn, "/api/tasks/#{task.id}"
 
     assert response(conn, 200) == task_as_json
+  end
+
+  test "POST /api/tasks creates a new task" do
+    task_as_json = %Task{title: "Walk the dog"}
+      |> Poison.encode!
+
+    conn = conn
+      |> put_req_header("content-type", "application/json")
+      |> post "/api/tasks", task_as_json
+
+    assert response(conn, 201) == 'foo'
   end
 end
