7e04483e36edfde3eea58cdb261fa1fecb4b281e
Steve Hook <steve.hook@gmail.com>
2016-04-13 19:01:32 +0100

    Failing test for login

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -11,6 +11,12 @@ defmodule Todo.SessionControllerTest do
 
   @tag :pending
   test "POST /api/sessions creates a new session given valid credentials" do
+    user = create_user
+    conn = conn
+    |> put_req_header("content-type", "application/json")
+    |> post("/api/session", %{email: "bob@example.com", password: "secret"})
+
+    assert json_response(conn, 201)
   end
 
   @tag :pending
