274733dcb2ff31dc15ac072a188d53a25ebba0a9
Steve Hook <steve.hook@gmail.com>
2016-06-28 08:40:39 +0100

    Fix specs

--- test/controllers/task_controller_test.exs
+++ test/controllers/task_controller_test.exs
@@ -9,23 +9,28 @@ defmodule Todo.TaskControllerTest do
     task
   end
 
-  test "GET /api/tasks returns a list of tasks" do
-    tasks_as_json = create_task
+  def create_tasks_as_json do
+    create_task
     |> List.wrap
     |> Poison.encode!
+  end
 
+  test "GET /api/tasks returns a list of tasks" do
+    tasks_as_json = create_tasks_as_json
     conn = get authenticated_conn, "/api/tasks"
-
     assert response(conn, 200) == tasks_as_json
+  end
+
+  test "GET /api/tasks requires authentication" do
+    create_tasks_as_json
+    conn = get conn, "/api/tasks"
+    assert response(conn, 422)
   end
 
   test "GET /api/tasks/:id returns a single task" do
     task = create_task
-    task_as_json = task
-    |> Poison.encode!
-
+    task_as_json = task |> Poison.encode!()
     conn = get authenticated_conn, "/api/tasks/#{task.id}"
-
     assert response(conn, 200) == task_as_json
   end
 
