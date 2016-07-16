1a5a0dd69ce42b7a85e1ba6c9f43eac413fcd5b6
Steve Hook <steve.hook@gmail.com>
2016-07-16 21:14:13 +0100

    Create a `Team` model

---
+++ web/models/team.ex
@@ -0,0 +1,32 @@
+defmodule Todo.Team do
+  use Todo.Web, :model
+
+  schema "users" do
+    field :name, :string
+    timestamps
+  end
+
+  @required_fields ~w(name)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
+  defimpl Poison.Encoder, for: Todo.Team do
+    def encode(user, _options) do
+      user
+      |> Map.from_struct
+      |> Map.drop([:__meta__, :__struct__])
+      |> Poison.encode!
+    end
+  end
+end
+
