5f3709e2a6e25082beb33bc429bbc26f216ecc19
Steve Hook <steve.hook@gmail.com>
2016-08-22 22:26:13 +0100

    Implement query to fix the test

--- web/controllers/team_controller.ex
+++ web/controllers/team_controller.ex
@@ -6,8 +6,13 @@ defmodule Todo.TeamController do
   plug Guardian.Plug.EnsureAuthenticated, [handler: Todo.SessionController]
 
   def index(conn, _params) do
-    # TODO: Get the current user, preload it's teams and return them
-    teams = Repo.all(Team)
+    user = Guardian.Plug.current_resource(conn)
+
+    query = from t in Team,
+      join: u in assoc(t, :users),
+      where: u.id == ^user.id
+
+    teams = Repo.all(query)
     conn
     |> put_status(200)
     |> json(teams)
