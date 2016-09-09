a772bd50fec9d7f56f84805d7620e355da1375be
Steve Hook <steve.hook@gmail.com>
2016-09-09 20:40:28 +0100

    Fix delete task tests

--- web/controllers/task_controller.ex
+++ web/controllers/task_controller.ex
@@ -116,18 +116,31 @@ defmodule Todo.TaskController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    task = Repo.get!(Task, id)
-    case Repo.delete(task) do
-      {:ok, task} ->
+  def delete(conn, %{"project_id" => project_id, "id" => id}) do
+    case load_project(project_id, conn) do
+      nil ->
         conn
-        |> put_status(200)
-        |> json(task)
-      {:error, changeset} ->
-        errors = errors_for_json(changeset.errors)
-        conn
-        |> put_status(422)
-        |> json(%{ "errors" => errors })
+        |> put_status(404)
+        |> json(%{})
+      project ->
+        case Repo.get_by(Task, %{id: id, project_id: project.id}) do
+          nil ->
+            conn
+            |> put_status(404)
+            |> json(%{})
+          task ->
+            case Repo.delete(task) do
+              {:ok, task} ->
+                conn
+                |> put_status(200)
+                |> json(task)
+              {:error, changeset} ->
+                errors = errors_for_json(changeset.errors)
+                conn
+                |> put_status(422)
+                |> json(%{ "errors" => errors })
+            end
+        end
     end
   end
 
