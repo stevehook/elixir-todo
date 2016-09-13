dfaff6c9f75e835ba860867b3cc45081006a13a9
Steve Hook <steve.hook@gmail.com>
2016-09-13 20:45:09 +0100

    Add tests for GET /api/project/:id

--- test/controllers/project_controller_test.exs
+++ test/controllers/project_controller_test.exs
@@ -3,37 +3,90 @@ defmodule Todo.ProjectControllerTest do
   alias Todo.Project
   alias Todo.Repo
 
-  def create_project(name) do
+  def create_project(name, user \\ nil) do
     { :ok, project } = %Project{name: name}
     |> Repo.insert
+
+    if user do
+      project
+      |> Repo.preload(:users)
+      |> Ecto.Changeset.change
+      |> Ecto.Changeset.put_assoc(:users, [user])
+      |> Repo.update!
+    end
+
     project
   end
 
   def create_projects_as_json(user) do
     create_project("Office workers")
 
-    house_workers = create_project("House workers")
-    |> Repo.preload(:users)
-    |> Ecto.Changeset.change
-    |> Ecto.Changeset.put_assoc(:users, [user])
-    |> Repo.update!
+    house_workers = create_project("House workers", user)
 
     house_workers
     |> List.wrap
     |> Poison.encode!
   end
 
-  test "GET /api/projects returns a list of current user projects" do
-    user = create_user
-    projects_as_json = create_projects_as_json(user)
-    conn = get authenticated_conn(user), "/api/projects"
-    assert response(conn, 200) == projects_as_json
+  def create_project_as_json(user) do
+    house_workers = create_project("House workers", user)
+    house_workers
+    |> Poison.encode!
   end
 
-  test "GET /api/projects returns 401 is not authenticated" do
-    user = create_user
-    create_projects_as_json(user)
-    conn = get build_conn, "/api/projects"
-    assert response(conn, 422)
+  describe "GET /api/projects" do
+    test "returns a list of current user projects" do
+      user = create_user
+      projects_as_json = create_projects_as_json(user)
+      conn = get authenticated_conn(user), "/api/projects"
+      assert response(conn, 200) == projects_as_json
+    end
+
+    test "returns 422 if not authenticated" do
+      user = create_user
+      create_projects_as_json(user)
+      conn = get build_conn, "/api/projects"
+      assert response(conn, 422)
+    end
+  end
+
+  describe "POST /api/projects" do
+  end
+
+  describe "GET /api/projects/:id" do
+    test "returns a given projects" do
+      user = create_user
+      project = create_project("My project", user)
+      project_as_json = project |> Poison.encode!
+      conn = get authenticated_conn(user), "/api/project/#{project.id}"
+      assert response(conn, 200) == project_as_json
+    end
+
+    test "returns 422 if not authenticated" do
+      user = create_user
+      project = create_project("My project", user)
+      conn = get conn, "/api/project/#{project.id}"
+      assert response(conn, 422)
+    end
+
+    test "returns 404 if project missing" do
+      user = create_user
+      project = create_project("My project", user)
+      conn = get authenticated_conn(user), "/api/project/#{project.id + 10}"
+      assert response(conn, 404)
+    end
+
+    test "returns 404 if user is not a member of the project" do
+      user = create_user
+      project = create_project("My project", user)
+      conn = get authenticated_conn(user), "/api/project/#{project.id}"
+      assert response(conn, 404)
+    end
+  end
+
+  describe "PATCH /api/projects/:id" do
+  end
+
+  describe "DELETE /api/projects/:id" do
   end
 end
